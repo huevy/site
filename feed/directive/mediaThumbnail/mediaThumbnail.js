954ed57789f384cd00eca617bb34de3e39d4c988
Huevy <huyovy@gmail.com>
2014-05-26 14:07:11 +0300

    photo beta

--- feed/directive/mediaThumbnail/mediaThumbnail.js
+++ feed/directive/mediaThumbnail/mediaThumbnail.js
@@ -8,8 +8,10 @@ angular.module('feed').directive('mediaThumbnail', function() {
     },
     templateUrl: 'feed/directive/mediaThumbnail/mediaThumbnail.html',
     link: function(scope, element, attrs, fn) {
-
-
+      var img = element.find('img');
+      var w = element.width();
+      var h = 0.75 * w;
+      element.css('height', h);
     }
   };
 });
